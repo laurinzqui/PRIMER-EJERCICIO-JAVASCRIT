d5ad54f47de258aba3bc38ff0f12486e78c3b2ee
Laura Solorio <laurinzqui@gmail.com>
2023-05-22 18:19:41 -0600

    se agrega nuevo ejercicio de javascript

---
+++ ejercicioTiposDeDatos.js
@@ -0,0 +1,86 @@
+//booleanos
+lauraEsAlta = false; 
+lauraEsMiNombre = true;
+console.log(lauraEsAlta);
+console.log(lauraEsMiNombre);
+
+//numeros
+primerNumero = 0;
+segundoNumero = 1; 
+console.log(primerNumero);
+console.log(segundoNumero);
+//strings
+primerString = "0";
+segundaString ="000";
+tercerString ="1";
+
+console.log(primerString);
+console.log(segundaString);
+console.log(tercerString);
+//NaN
+notAnumber = 0/0;
+console.log(notAnumber);
+
+//infity
+infinityMas = 5/0;
+ininityMenos = 0-0;
+
+console.log(infinityMas);
+console.log(ininityMenos);
+
+
+//null
+salario = null;
+
+console.log(salario);
+
+//más strings
+veinte ="20";
+twenty ="Twenty";
+
+console.log(veinte);
+console.log(twenty);
+
+
+//null
+nuevoNull = null; 
+
+console.log(nuevoNull);
+
+
+
+//undefined
+noDefinido  = undefined;
+console.log(noDefinido);
+
+//typeof
+console.log(typeof(lauraEsAlta));
+console.log(typeof(lauraEsMiNombre));
+console.log(typeof(primerNumero));
+console.log(typeof(segundoNumero));
+console.log(typeof(primerString));
+console.log(typeof(segundaString));
+console.log(typeof(tercerString));
+console.log(typeof(notAnumber));
+console.log(typeof(infinityMas));
+console.log(typeof(ininityMenos));
+console.log(typeof(salario));
+console.log(typeof(veinte));
+console.log(typeof(twenty));
+console.log(typeof(nuevoNull));
+console.log(typeof(noDefinido));
+
+
+//muñecas y payasos venta
+ numMuñecas = 14;
+ numPayasos= 27; 
+  pesoMuñeca = 75; 
+  pesoPayaso = 112;
+  totalPesoMuñecas = numMuñecas*pesoMuñeca; 
+  totalPesoPayasos= numPayasos*pesoPayaso;
+  totalPedido = totalPesoMuñecas + totalPesoPayasos;
+console.log(totalPedido);
+
+
+
+
